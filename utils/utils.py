510826f62e579adb11127799d1b4ee361ff8c72d
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-26 15:22:36 +1000

    add image padding

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,19 @@
-import tensorflow as tf
+from PIL import Image
+import numpy as np
 import os
 
-PATH = os.path.dirname(os.path.realpath(__file__))
+
+def image_padding(image, label, image_height, image_width):
+    shape = image.shape
+    assert image_height >= shape[0]
+    assert image_width >= shape[1]
+
+    offset_x, offset_y = 0, 0
+
+    new_image = np.zeros([image_height, image_width, 3])
+    new_image[offset_x: offset_x + shape[0], offset_y: offset_y + shape[1]] = image
+
+    new_label = np.zeros([image_height, image_width])
+    new_label[offset_x: offset_x + shape[0], offset_y: offset_y + shape[1]] = label
+
+    return new_image, new_label
