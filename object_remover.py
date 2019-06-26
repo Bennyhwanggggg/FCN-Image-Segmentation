3455cbf582a6a5a7fe3d8387604871fb59f179ff
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-26 15:16:54 +1000

    restrucutre project

--- object_remover.py
+++ object_remover.py
@@ -1,8 +1,8 @@
+import os
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import os
-from utils import *
-from model import *
+from models.model import *
+from utils.utils import *
 
 PATH = os.path.dirname(os.path.realpath(__file__))
