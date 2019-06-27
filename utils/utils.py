8699327fe05daa986ba3b952489f5c6b45b62405
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-27 14:11:00 +1000

    complete utils onehot incoding

--- utils/utils.py
+++ utils/utils.py
@@ -17,3 +17,10 @@ def image_padding(image, label, image_height, image_width):
     new_label[offset_x: offset_x + shape[0], offset_y: offset_y + shape[1]] = label
 
     return new_image, new_label
+
+
+def one_hot_init_v2(dp, num_class):
+    out = np.zeros((dp.size, num_class), dtype=np.uint8)
+    out[np.arange(dp.size), dp.ravel().astype('uint8')] = 1
+    out.shape = dp.shape + (num_class,)
+    return out
