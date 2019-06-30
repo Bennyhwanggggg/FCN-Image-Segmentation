9474393d216bfafefec07bcae967ba482dcdbbbb
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-30 19:42:22 +1000

    constructor setup in fcn

--- object_remover.py
+++ object_remover.py
@@ -6,3 +6,69 @@ from models.model import *
 from utils.utils import *
 
 PATH = os.path.dirname(os.path.realpath(__file__))
+training_data_directory = 'Training_data/data_png'
+training_mask_data_directory = 'Training_data/mask_bin'
+testing_data_directory = 'Test_data/data_png'
+testing_mask_data_directory = 'Test_data/mask_bin'
+
+
+class ObjectRemover:
+    def __init__(self, learning_rate=1e-6, train_epochs=1000, batch_size=1, eval_step=50):
+        """
+        Initiate Object Remove that uses FCN (Fully Convolutional Network) to detect
+        desired objects in an image that needs to be removed.
+
+        :param learning_rate: learning rate to use
+        :param train_epochs: number of training epochs
+        :param batch_size: batch size to use
+        :param eval_step: evaluation steps
+        """
+        self.original_height = 1280
+        self.original_width = 720
+        self.image_height = int(np.ceil(self.original_height/32) * 32)
+        self.image_width = int(np.ceil(self.original_width/32) * 32)
+
+        """
+        Neural Network Setup
+        """
+        self.learning_rate = learning_rate
+        self.training_epochs = train_epochs
+        self.batch_size = batch_size
+        self.eval_step = eval_step
+        self.test_batch = 1
+
+        """
+        Directories Setup
+        """
+        self.training_directory = os.path.join(PATH, training_data_directory)
+        self.training_mask_directory = os.path.join(PATH, training_mask_data_directory)
+
+        self.test_data_directory = os.path.join(PATH, testing_data_directory)
+        self.test_mask_directory = os.path.join(PATH, testing_mask_data_directory)
+
+        """
+        Training and test parameter setup
+        """
+        self.training_data, self.traning_mask_gt = self.create_training_data(num_class=2)
+        self.test_data, self.test_mask_gt = self.create_test_data(num_class=2)
+        self.batch_num = self.training_data.shape[0]//self.batch_size
+        self.test_batch_num = self.test_data.shape[0]//self.test_batch
+
+    def create_training_data(self, num_class=2):
+        training_data, training_mask_gt = create_data(self.training_directory,
+                                                      self.training_mask_directory,
+                                                      self.image_height,
+                                                      self.image_width,
+                                                      num_class)
+        return training_data, training_mask_gt
+
+    def create_test_data(self, num_class=2):
+        test_data, test_mask_gt = create_data(self.test_data_directory,
+                                              self.test_mask_directory,
+                                              self.image_height,
+                                              self.image_width,
+                                              num_class)
+        return test_data, test_mask_gt
+
+
+
